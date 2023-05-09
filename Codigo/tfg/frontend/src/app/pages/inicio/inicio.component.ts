94af755013941a1a17e10597ce140e455d5c7e9a
Pablo Garcia Carbonell <pgc69@gcloud.ua.es>
2023-05-09 19:04:43 +0200

    Empezando con las interfaces

--- Codigo/tfg/frontend/src/app/pages/inicio/inicio.component.ts
+++ Codigo/tfg/frontend/src/app/pages/inicio/inicio.component.ts
@@ -13,15 +13,15 @@ import Swal from 'sweetalert2';
 })
 
 export class InicioComponent implements OnInit {
-  public datosP = this.f.group({
+  public datosP: any = this.f.group({
     texto: ['', Validators.required],
     edad: ['',  Validators.required], 
     objetivo:['',  Validators.required]
   });
 
   public listaLugares:any = [];  
-  public mensaje = '<h3>Rellena toda la información</h3>';
-
+  public pintarCat = -1;
+  public pintarEdad = -1;
 
   constructor( private f: FormBuilder, private envio: EnvioService, private router: Router) { }
 
@@ -55,7 +55,54 @@ export class InicioComponent implements OnInit {
         });
     }
   }
+
   getLugares(){
     return this.listaLugares;
+  }
+
+  seleccionarBoton(valor: any){
+    switch(valor){
+      case 0: 
+        this.datosP.get('objetivo').setValue('comida');
+        this.pintarCat = 0; 
+      break;
+      case 1: 
+        this.datosP.get('objetivo').setValue('cultura');
+        this.pintarCat = 1;
+      break;
+      case 2: 
+        this.datosP.get('objetivo').setValue('en familia');
+        this.pintarCat = 2;
+      break;
+      case 3: 
+        this.datosP.get('objetivo').setValue('deporte');
+        this.pintarCat = 3;
+      break;
+    }
+  }
+
+  seleccionarEdad(valor:any){
+    switch(valor){
+      case 0:
+        this.datosP.get('edad').setValue(20);
+        this.pintarEdad = 0; 
+      break;
+      case 1:
+        this.datosP.get('edad').setValue(30);
+        this.pintarEdad = 1; 
+      break;
+      case 2: 
+        this.datosP.get('edad').setValue(40);
+        this.pintarEdad = 2; 
+      break;
+      case 3: 
+        this.datosP.get('edad').setValue(60);
+        this.pintarEdad = 3; 
+      break;
+      case 4: 
+        this.datosP.get('edad').setValue(65);
+        this.pintarEdad = 4; 
+      break;
+    }
   }
 }
